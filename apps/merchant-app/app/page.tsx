a560c901d27cace7494a5fa2b295aa8a2603f966
Rachit123kumar <hellobittukumar12@gmail.com>
2024-08-30 08:13:05 +0530

    first commit

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="bg-red-200">
+      Hii There This is merchant  app 
+    </div>
+  );
+}
---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="bg-red-200">
+      Hii There This is user app 
+    </div>
+  );
+}
